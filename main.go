10e19dd97eaf033e917d3d5230a2c4d823ddcef2
Mark Bailey <mark.bailey@moyerautogroup.com>
2023-09-22 10:18:08 -0400

    Update main.go

--- main.go
+++ main.go
@@ -1,5 +1,24 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"os"
+
+	tea "github.com/charmbracelet/bubbletea"
+
+	"gooradio/views/startscreen"
+)
+
 func main() {
 	// TODO: Implement
+	m, err := startscreen.NewStartScreen()
+	if err != nil {
+		fmt.Println(fmt.Sprintf("Error: %v", err))
+		os.Exit(1)
+	}
+
+	if _, err := tea.NewProgram(m).Run(); err != nil {
+		log.Fatal(err)
+	}
 }
